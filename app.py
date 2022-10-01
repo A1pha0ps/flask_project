d6ce3f1ed5b96f8456a257abd4c7bf4c05205064
A1pha <majorzhen@gmail.com>
2022-10-01 14:58:56 -0400

    test

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+# No cacheing at all for API endpoints.
+@app.after_request
+def add_header(response):
+    response.cache_control.no_store = True
+    if 'Cache-Control' not in response.headers:
+        response.headers['Cache-Control'] = 'no-store'
+    return response
 
 # Create the receiver API POST endpoint:
-
-
 @app.route("/receiver", methods=["POST"])
 def postME():
     data = request.get_json()
